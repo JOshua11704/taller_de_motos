e12a861dea7e5fefa9d411a4bff23dd2f0f159ec
Joshua Ortiz <jortiz336@misena.edu.co>
2023-08-03 06:45:23 -0500

    database

--- motostaller.sql
+++ motostaller.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2023 a las 08:50:26
+-- Tiempo de generación: 03-08-2023 a las 13:44:54
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,6 +32,18 @@ CREATE TABLE `cilindraje` (
   `cilindraje` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `cilindraje`
+--
+
+INSERT INTO `cilindraje` (`id_cilindraje`, `cilindraje`) VALUES
+(1, '150'),
+(2, '250'),
+(3, '150'),
+(4, '250'),
+(5, '100'),
+(6, '200');
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +55,17 @@ CREATE TABLE `color` (
   `color` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `color`
+--
+
+INSERT INTO `color` (`id_color`, `color`) VALUES
+(1, 'Blanco'),
+(2, 'Negro'),
+(3, 'Azul'),
+(4, 'Plateado'),
+(5, 'Amarrillo');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +76,14 @@ CREATE TABLE `combustible` (
   `id_combustible` int(11) NOT NULL,
   `combustible` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `combustible`
+--
+
+INSERT INTO `combustible` (`id_combustible`, `combustible`) VALUES
+(1, 'Diesel'),
+(2, 'Gasolina');
 
 -- --------------------------------------------------------
 
@@ -195,7 +226,14 @@ INSERT INTO `fecha` (`id_fecha`, `fecha`, `hora`, `document`) VALUES
 (27, '2023-04-27', '17:45:57', 1110451633),
 (28, '2023-04-29', '14:16:02', 1110451633),
 (29, '2023-05-01', '18:30:13', 1110451633),
-(30, '2023-05-02', '00:02:07', 1110451633);
+(30, '2023-05-02', '00:02:07', 1110451633),
+(31, '2023-07-31', '16:16:04', 1110451633),
+(32, '2023-08-01', '16:30:21', 1110451633),
+(33, '2023-08-01', '16:30:23', 1110451633),
+(34, '2023-08-02', '11:28:14', 1110451633),
+(35, '2023-08-02', '17:59:16', 1110451633),
+(36, '2023-08-02', '20:31:09', 1110451633),
+(37, '2023-08-03', '06:32:28', 1110451633);
 
 -- --------------------------------------------------------
 
@@ -208,6 +246,14 @@ CREATE TABLE `linea` (
   `linea` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `linea`
+--
+
+INSERT INTO `linea` (`id_linea`, `linea`) VALUES
+(1, '125NKD EIII'),
+(2, '513NGA EII');
+
 -- --------------------------------------------------------
 
 --
@@ -219,6 +265,14 @@ CREATE TABLE `marca` (
   `marca` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`id_marca`, `marca`) VALUES
+(1, 'AKT'),
+(2, 'BMW');
+
 -- --------------------------------------------------------
 
 --
@@ -229,6 +283,22 @@ CREATE TABLE `modelo` (
   `id_modelo` int(11) NOT NULL,
   `modelo` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `modelo`
+--
+
+INSERT INTO `modelo` (`id_modelo`, `modelo`) VALUES
+(1, '2010'),
+(2, '2022'),
+(3, '2021'),
+(4, '2023'),
+(5, '2011'),
+(6, '2012'),
+(7, '2012'),
+(8, '2013'),
+(9, '2014'),
+(10, '2015');
 
 -- --------------------------------------------------------
 
@@ -246,7 +316,6 @@ CREATE TABLE `moto` (
   `id_modelo` int(11) NOT NULL,
   `id_cilindraje` int(11) NOT NULL,
   `id_color` int(11) NOT NULL,
-  `id_rol` int(11) NOT NULL,
   `id_clase` int(11) NOT NULL,
   `id_carroseria` int(11) NOT NULL,
   `capacidad` tinyint(2) NOT NULL,
@@ -255,6 +324,18 @@ CREATE TABLE `moto` (
   `vin` varchar(20) NOT NULL,
   `numero_chasis` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `moto`
+--
+
+INSERT INTO `moto` (`placa`, `id_marca`, `descripcion`, `documento`, `km`, `id_linea`, `id_modelo`, `id_cilindraje`, `id_color`, `id_clase`, `id_carroseria`, `capacidad`, `id_combustible`, `numero_motor`, `vin`, `numero_chasis`) VALUES
+('23r223', 1, 'efbfeefs wfwf w ', 1110451633, 2343, 2, 6, 1, 3, 1, 2, 2, 2, 'rywy732', '1241hegr', '63434g33g'),
+('4QWRQ', 1, 'QDWQDQ DQQ', 123345, 23, 1, 1, 1, 1, 1, 1, 2, 1, 'werwew2134', '35423fw2e', '3242d'),
+('hwr6234', 2, 'fg nfgjtjrtj', 31231414, 2362, 1, 5, 4, 1, 1, 1, 1, 2, 'rywy734', '1241hegrwg', 'g3er42'),
+('Mf45KD', 2, 'hyrmgbrvftg', 1110451633, 234, 2, 2, 3, 1, 2, 2, 2, 1, '362gwgw13', '2412wetrw', '63434g3g3gdfg'),
+('msd462', 2, 'hetehthetr', 123345, 234, 2, 5, 6, 1, 1, 1, 2, 1, 'rywy734', '24f3f2g24', 'g3er42'),
+('y3yy33', 1, 'r230rj23rj2 r2 r2r0 ', 1110451633, 124, 1, 7, 3, 3, 2, 2, 3, 1, 'rj2r2r0 ', '23rt32fd', '13r123rt');
 
 -- --------------------------------------------------------
 
@@ -332,6 +413,14 @@ CREATE TABLE `tipo_carroceria` (
   `carroseria` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `tipo_carroceria`
+--
+
+INSERT INTO `tipo_carroceria` (`id_carroseria`, `carroseria`) VALUES
+(1, 'Sin carroceria'),
+(2, 'Con carroceria');
+
 -- --------------------------------------------------------
 
 --
@@ -353,6 +442,14 @@ CREATE TABLE `tipo_vehiculo` (
   `id_clase` int(11) NOT NULL,
   `tip_vehiculo` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_vehiculo`
+--
+
+INSERT INTO `tipo_vehiculo` (`id_clase`, `tip_vehiculo`) VALUES
+(1, 'Motocicleta'),
+(2, 'Bicicleta');
 
 -- --------------------------------------------------------
 
@@ -490,16 +587,15 @@ ALTER TABLE `modelo`
 --
 ALTER TABLE `moto`
   ADD PRIMARY KEY (`placa`),
-  ADD KEY `id_carroseria` (`id_carroseria`),
-  ADD KEY `id_cilindraje` (`id_cilindraje`),
-  ADD KEY `id_clase` (`id_clase`),
-  ADD KEY `id_color` (`id_color`),
-  ADD KEY `id_combustible` (`id_combustible`),
-  ADD KEY `id_linea` (`id_linea`),
   ADD KEY `id_marca` (`id_marca`),
+  ADD KEY `documento` (`documento`),
+  ADD KEY `id_linea` (`id_linea`),
   ADD KEY `id_modelo` (`id_modelo`),
-  ADD KEY `id_rol` (`id_rol`),
-  ADD KEY `documento` (`documento`);
+  ADD KEY `id_cilindraje` (`id_cilindraje`),
+  ADD KEY `id_color` (`id_color`),
+  ADD KEY `moto_ibfk_7` (`id_clase`),
+  ADD KEY `moto_ibfk_8` (`id_carroseria`),
+  ADD KEY `id_combustible` (`id_combustible`);
 
 --
 -- Indices de la tabla `repuestos`
@@ -551,10 +647,46 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cilindraje`
+--
+ALTER TABLE `cilindraje`
+  MODIFY `id_cilindraje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `color`
+--
+ALTER TABLE `color`
+  MODIFY `id_color` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `combustible`
+--
+ALTER TABLE `combustible`
+  MODIFY `id_combustible` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `fecha`
 --
 ALTER TABLE `fecha`
-  MODIFY `id_fecha` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id_fecha` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de la tabla `linea`
+--
+ALTER TABLE `linea`
+  MODIFY `id_linea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `modelo`
+--
+ALTER TABLE `modelo`
+  MODIFY `id_modelo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `repuestos`
@@ -567,6 +699,24 @@ ALTER TABLE `repuestos`
 --
 ALTER TABLE `servicio`
   MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_carroceria`
+--
+ALTER TABLE `tipo_carroceria`
+  MODIFY `id_carroseria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_servicio`
+--
+ALTER TABLE `tipo_servicio`
+  MODIFY `id_tip_servicio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_vehiculo`
+--
+ALTER TABLE `tipo_vehiculo`
+  MODIFY `id_clase` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -611,16 +761,15 @@ ALTER TABLE `factura_venta`
 -- Filtros para la tabla `moto`
 --
 ALTER TABLE `moto`
-  ADD CONSTRAINT `moto_ibfk_1` FOREIGN KEY (`id_carroseria`) REFERENCES `tipo_carroceria` (`id_carroseria`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_10` FOREIGN KEY (`documento`) REFERENCES `usuario` (`documento`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_2` FOREIGN KEY (`id_cilindraje`) REFERENCES `cilindraje` (`id_cilindraje`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_3` FOREIGN KEY (`id_clase`) REFERENCES `tipo_vehiculo` (`id_clase`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_4` FOREIGN KEY (`id_color`) REFERENCES `color` (`id_color`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_5` FOREIGN KEY (`id_combustible`) REFERENCES `combustible` (`id_combustible`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_6` FOREIGN KEY (`id_linea`) REFERENCES `linea` (`id_linea`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_7` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_8` FOREIGN KEY (`id_modelo`) REFERENCES `modelo` (`id_modelo`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `moto_ibfk_9` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `moto_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_2` FOREIGN KEY (`documento`) REFERENCES `usuario` (`documento`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_3` FOREIGN KEY (`id_linea`) REFERENCES `linea` (`id_linea`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_4` FOREIGN KEY (`id_modelo`) REFERENCES `modelo` (`id_modelo`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_5` FOREIGN KEY (`id_cilindraje`) REFERENCES `cilindraje` (`id_cilindraje`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_6` FOREIGN KEY (`id_color`) REFERENCES `color` (`id_color`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_7` FOREIGN KEY (`id_clase`) REFERENCES `tipo_vehiculo` (`id_clase`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_8` FOREIGN KEY (`id_carroseria`) REFERENCES `tipo_carroceria` (`id_carroseria`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `moto_ibfk_9` FOREIGN KEY (`id_combustible`) REFERENCES `combustible` (`id_combustible`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `repuestos`
