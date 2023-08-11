59478ef10231577d0674db53f0b348331bf9a282
Joshua Ortiz <jortiz336@misena.edu.co>
2023-08-10 22:02:18 -0500

    menos las ventas

--- vista/gmail.php
+++ vista/gmail.php
@@ -6,5 +6,19 @@
  $db = new Database();
  $conectar = $db -> conectar();
 
+
+$correo ="jortiz336@misena.edu.co";
+$title ="Prueba";
+$cuerpo ="Vamos a probar si sirve tu correo";
+$myemail = "From: joshuaortizgaitan2004@gmail.com";
+
+if(mail($correo, $title, $cuerpo, $myemail))
+{
+    echo "Enviado con Éxito";
+}else{
+    echo "Error al enviar el mensaje";
+}
+
+
 ?>
 
